2dd7b67d7f661455651a39b5e3cfb006db431235
surekhaBhas <tsurekha3@gmail.com>
2023-09-13 12:22:17 +0530

    updated

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ app.use('/register',require('./routes/register'))
 app.use('/login',require('./routes/login'))
 app.use('/refresh',require('./routes/refresh'))
 app.use('/logout',require('./routes/logout'))
-app.use(verifyJWT)
 app.use('/locations', require('./routes/locations'));
 app.use('/cities', require('./routes/city'));
+app.use(verifyJWT)
+
+
 app.use('/filter', require('./routes/filter'));
 app.use('/quickSearch', require('./routes/quickSearch'));
 app.use('/restaurant', require('./routes/restaurant'));
