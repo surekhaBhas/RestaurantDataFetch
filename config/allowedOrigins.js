ad2f143359ab60ca878b232d9bb639c62d2ac38f
surekhaBhas <tsurekha3@gmail.com>
2023-09-16 18:44:45 +0530

    updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    "https://app.netlify.com/"
+    "https://app.netlify.com/",
+    'https://6505a3a0fbb37b168ecf0bb2--jazzy-palmier-2111ae.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
